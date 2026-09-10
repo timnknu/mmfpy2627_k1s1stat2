a1512ccb683e07f72c46bd7d9394cc44328b2402
Your Name <you@example.com>
2026-09-10 09:29:21 +0300

    ex1

---
+++ 01_sep10/t01.py
@@ -0,0 +1,14 @@
+# Складіть програму взаємного обміну значень цілих змінних x та y.
+
+x = 10
+y = 5
+
+print( x )
+print( "var y is:", y )
+
+t1 = x
+x = y
+y = t1
+
+print("x equals", x)
+print("y equals", y)
